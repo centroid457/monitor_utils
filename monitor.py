461401080012d559a8fc16aa66288bb03c402d30
admin <023>
2023-09-03 20:18:40 +0300

    full ref EnvironsOsGetterClass

--- monitor.py
+++ monitor.py
@@ -1,5 +1,7 @@
+# TODO: need sav elast value! maybe in file!
+# TODO: move EnvironsOsGetterClass in separate module!
 """
-before using add _SmtpSender.ENVS_REQUIRED into your OS!
+before using add _SmtpSender.ENVirons into your OS!
 """
 
 import os
@@ -17,43 +19,75 @@ import smtplib
 
 
 # =====================================================================================================================
-class _Environs:
+class EnvironsOsGetterClass:
     """
-    get environs from OS!
-    if not exists some of them - RAISE!
+    get environs from OS or use default!
+    if not exists some value of them - RAISE!
+
+    # add all ENVS with type STR!
+
+    firstly it will try to get value from OsEnv
+    then if not will be used already seted as default!
+
+    define env NAMES
+        ENV__MAIL_USER: str = None  # will find ENV__MAIL_USER/MAIL_USER
+        ENV__myEnv: str = None  # will find ENV__myEnv/myEnv
+        MAIL_USER: str = None  # this is not expected as env!
+
+    default VALUES
+        ENV__MAIL_USER: str = None      # no default value
+        ENV__MAIL_USER: str = "hello"   # def value set!
     """
-    # OVERWRITING NEXT -------------------------------
-    ENVS_REQUIRED: List[str] = []
-    """
-    # add all ENVS with types STR!!! so usable in IDE!!!
-    # EXAMPLE:
-    ENVS_REQUIRED = ["ENV__MAIL_USER", "ENV__MAIL_PWD"]
-    ENV__MAIL_USER: str
-    ENV__MAIL_PWD: str
-    """
+
+    __env_name__prefix: str = "ENV__"
 
     def __init__(self):
         super().__init__()
 
-        for environ_name in self.ENVS_REQUIRED:
-            environ_value = os.getenv(environ_name, None)
-            if environ_value is not None:
-                setattr(self, environ_name, environ_value)
-            else:
-                msg = f"[CRITICAL]there is no {environ_name=} in OS! add it manually!!!"
+        self._envs_detected: Dict[str, Optional[str]] = {}     # just for debug
+
+        self.__env_names__detect()
+        self.__env_values__update_from_os()
+        self.__env_values__check_exists()
+
+    def __env_names__detect(self):
+        for name in dir(self):
+            if name.startswith(self.__env_name__prefix):
+                self._envs_detected.update({name: getattr(self, name)})
+
+    def __env_values__update_from_os(self):
+        for name_w_prefix in self._envs_detected:
+            name_wo_prefix = name_w_prefix.replace(self.__env_name__prefix, "", 1)
+
+            env_name__os = None
+
+            if name_w_prefix in os.environ:
+                env_name__os = name_w_prefix
+            elif name_wo_prefix in os.environ:
+                env_name__os = name_wo_prefix
+
+            if env_name__os:
+                env_value__os = os.getenv(env_name__os)
+                setattr(self, name_w_prefix, env_value__os)
+
+                self._envs_detected.update({name_w_prefix: env_value__os})
+
+    def __env_values__check_exists(self):
+        for name in self._envs_detected:
+            if getattr(self, name) is None:
+                msg = f"[CRITICAL]there is no [{name=}] in OsEnvs and not exists default value! add it manually!!!"
                 print(msg)
                 raise Exception(msg)
 
 
 # =====================================================================================================================
-class _SmtpSender(_Environs):
+class _SmtpSender(EnvironsOsGetterClass):
     """
     main class to work with smtp.
     """
     # OVERWRITTEN NOW -------------------------------
-    ENVS_REQUIRED: List[str] = ["ENV__MAIL_USER", "ENV__MAIL_PWD"]
-    ENV__MAIL_USER: str     # example@mail.ru
-    ENV__MAIL_PWD: str      # use thirdPartyPwd!
+    ENV__MAIL_USER: str = None    # example@mail.ru
+    ENV__MAIL_PWD: str = None     # use thirdPartyPwd!
 
     # OVERWRITING NEXT -------------------------------
     SMTP_SERVER = "smtp.mail.ru"
