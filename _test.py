e1987db7610d671f3801af0e0d8ff83f551d9f0d
admin <023>
2023-09-18 00:37:47 +0300

    make autotest for imap

--- _test.py
+++ _test.py
@@ -7,10 +7,11 @@ from typing import *
 import threading
 
 from monitor_utils import *
+from alerts_msg import *
 
 
 # =====================================================================================================================
-# TODO: create better/auto test!
+# TODO: create full auto test!
 
 
 # =====================================================================================================================
@@ -21,9 +22,23 @@ def test__url_tag():
 
     # Monitor_Sportmaster_AdidasSupernova2M()
 
+@pytest.mark.parametrize(argnames="pattern", argvalues=[None, r"\[ALERT\]test1"])
+def test__imap(pattern):
+    subj_suffix = "test1"
 
-def test__imap():
-    MonitorImap()
+    AlertSmtp(subj_suffix=subj_suffix)
+
+    victim = MonitorImap
+    victim.stop = True
+    victim.SUBJECT_REGEXP = pattern
+
+    for i in range(3):
+        victim_inst = victim()
+        victim_inst.wait_step(1)
+        if f"[ALERT]{subj_suffix}" in victim_inst._detected:
+            break
+
+    assert f"[ALERT]{subj_suffix}" in victim_inst._detected
 
 
 # =====================================================================================================================
