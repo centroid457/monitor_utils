2626bf58f4f73436bf9cb5ae29e06cc6dd37c187
Andrei Starichenko <centroid@mail.ru>
2024-05-22 12:32:57 +0300

    ready v0.0.6=[__INIT__.py] fix import+apply last pypi template

--- monitor_utils/__init__.py
+++ monitor_utils/__init__.py
@@ -1,3 +1,48 @@
-from .monitor_url_tag import *
-from .monitor_url_tag__implements import *
-from .monitor_imap import *
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+from .monitor_url_tag import (
+    # BASE
+    MonitorUrlTag,
+
+    # AUX
+    TagAddressChain,
+    Alert_MonitorUrlTag,
+
+    # TYPES
+
+    # EXX
+)
+from .monitor_url_tag__implements import (
+    # BASE
+    Monitor_DonorSvetofor,
+    Monitor_CbrKeyRate,
+    Monitor_ConquestS23_comments,
+    Monitor_Sportmaster_AdidasSupernova2M,
+    Monitor_AcraRaiting_GTLC,
+
+    # AUX
+
+    # TYPES
+
+    # EXX
+)
+from .monitor_imap import (
+    # BASE
+    MonitorImap,
+
+    # AUX
+    ImapAddress,
+    ImapServers,
+    AlertImap,
+
+    # TYPES
+
+    # EXX
+)
+
+# =====================================================================================================================
